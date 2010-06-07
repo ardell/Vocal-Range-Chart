f3b590c96d120611a9b83b37972563ab52d0e148
Jason Ardell <jason@patienttrail.com>
2010-06-06 21:30:39 -0400

    Changed validation messages for survey model.

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -4,18 +4,18 @@ class Survey < ActiveRecord::Base
   validates_uniqueness_of :owner_hash, :visitor_hash
   validates_format_of :email,
                       :with => %r{^.*@.*\..*$}i,
-                      :message => 'Invalid email address, please try again'
+                      :message => 'Invalid email address'
   validates_format_of :email,
                       :with => %r{^.{3,150}$}i,
-                      :message => 'Email must be between 3 and 150 characters, please try again'
+                      :message => 'Email must be between 3 and 150 characters'
   validates_format_of :group,
                       :with => %r{^.{3,150}$}i,
-                      :message => 'Name must be between 3 and 150 characters, please try again'
+                      :message => 'Group name must be between 3 and 150 characters'
   validates_format_of :owner_hash,
                       :with => %r{^[0-9a-f]{32}$}i,
-                      :message => 'We were unable to create your survey, please try again'
+                      :message => 'We were unable to create your survey'
   validates_format_of :visitor_hash,
                       :with => %r{^[0-9a-f]{32}$}i,
-                      :message => 'We were unable to create your survey, please try again'
+                      :message => 'We were unable to create your survey'
   
 end
